d3067805270c31cfd77a285083b026e13afe3e12
elloot <elliot.duchek@gmail.com>
2020-08-21 10:43:08 +0200

    fix

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ document.querySelector(".navburger").addEventListener("click", () => {
   document.querySelector(".menu").classList.toggle("showMenu")
 })
 
-document.querySelector(".klattring").addEventListener("click", () => {
+document.querySelector(".klattringLink").addEventListener("click", () => {
   document.querySelector(".klattring").classList.toggle("show")
 })
 
-document.querySelector(".personer").addEventListener("click", ()=>{
+document.querySelector(".personerLink").addEventListener("click", ()=>{
   document.querySelector(".personer").classList.toggle("show")
 })

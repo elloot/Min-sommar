8a66493ba49ad55acb54551c9623a2a1d448323e
elloot <elliot.duchek@gmail.com>
2020-08-21 10:39:01 +0200

    ih

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 document.querySelector(".navburger").addEventListener("click", () => {
   document.querySelector(".menu").classList.toggle("showMenu")
 })
+
+document.querySelector(".klattring").addEventListener("click", () => {
+  document.querySelector(".klattring").classList.toggle("show")
+})
+
+document.querySelector(".personer").addEventListener("click", ()=>{
+  document.querySelector(".personer").classList.toggle("show")
+})
